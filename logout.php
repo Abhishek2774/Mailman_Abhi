88408a3989b04b9d50268d72fc2ba2f6d463f818
Abhishek Kumar <75356361+errorkeda@users.noreply.github.com>
2022-07-25 18:16:37 +0530

    change url for server

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 session_start(); //to ensure you are using same session
 session_unset();
 session_destroy(); //destroy the session
-// header("location:http://hestalabs.com/tse/Abhishek_mailman/index.php"); //server
-header("location:http://localhost/Abhishek_mailman/index.php"); // local
+header("location:http://hestalabs.com/tse/Abhishek_mailman/index.php"); //server
+// header("location:http://localhost/Abhishek_mailman/index.php"); // local
 exit();
 ?>
